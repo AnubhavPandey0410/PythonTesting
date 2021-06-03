cd69b763377540993b29062d46e68c95ad8ae8a4
Anubhav <anubhav.pandey0410@gmail.com>
2021-06-03 15:45:40 +0530

    git remote add origin https://github.com/AnubhavPandey0410/Mycode.git

--- Methods.py
+++ Methods.py
@@ -28,9 +28,10 @@ class Chrome:
         self.driver.find_element_by_class_name('product_sort_container').click()
         self.driver.find_element_by_css_selector(
             "div.page_wrapper div.header_container:nth-child(1) div.header_secondary_container div.right_component span.select_container select.product_sort_container > option:nth-child(4)").click()
-        #self.driver.execute_script("window.scrollBy(0,1000)","")
-        tshirt=self.driver.find_element_by_xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[5]/div[2]/div[1]/a[1]/div[1]")
-        self.driver.execute_script("arguments[0].scrollIntoView();",tshirt)
+        # self.driver.execute_script("window.scrollBy(0,1000)","")
+        tshirt = self.driver.find_element_by_xpath(
+            "/html[1]/body[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[5]/div[2]/div[1]/a[1]/div[1]")
+        self.driver.execute_script("arguments[0].scrollIntoView();", tshirt)
         self.driver.find_element_by_id("add-to-cart-sauce-labs-onesie").click()
         self.driver.find_element_by_id("add-to-cart-sauce-labs-fleece-jacket").click()
         self.driver.find_element_by_css_selector(
@@ -39,18 +40,34 @@ class Chrome:
         self.driver.find_element_by_id("first-name").send_keys("Anubhav")
         self.driver.find_element_by_id("last-name").send_keys("Pandey")
         self.driver.find_element_by_id("postal-code").send_keys("201309")
-        #p1= self.driver.find_element_by_xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[3]/div[2]/div[2]").text
+        # p1= self.driver.find_element_by_xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[3]/div[2]/div[2]").text
         self.driver.find_element_by_id("continue").click()
-        number=self.driver.find_element_by_class_name("shopping_cart_badge")
+        number = self.driver.find_element_by_class_name("shopping_cart_badge")
         print(number)
-        Quantity=self.driver.find_element_by_class_name("cart_quantity")
+        Quantity = self.driver.find_element_by_class_name("cart_quantity")
         print(Quantity)
         self.driver.find_element_by_id("finish").click()
         self.driver.find_element_by_id("back-to-products").click()
         print("Thank you for shopping")
+
+    def sidebar(self):
         self.driver.find_element_by_css_selector("#react-burger-menu-btn").click()
-        self.driver.implicitly_wait(20000)
+        # self.driver.implicitly_wait(2000)
+        self.driver.find_element_by_id("about_sidebar_link").click()
+        self.driver.implicitly_wait(10000)
+        self.driver.find_element_by_id("onetrust-accept-btn-handler").click()
+        self.driver.find_element_by_xpath("//header/div[1]/nav[1]/ul[1]/li[3]/ul[1]/li[1]/a[1]").click()
+        # self.driver.find_element_by_class_name("A_a__3od2X").click()
+        self.driver.find_element_by_class_name("A_button__1Sovt").click()
+        i = 1
+        while i <= 3:
+            self.driver.back()
+            i += 1
+        else:
+            self.driver.find_element_by_css_selector("#react-burger-menu-btn").click()
         self.driver.find_element_by_css_selector("#logout_sidebar_link").click()
+        #self.driver.close()
+
 
     def user1(self):
         self.driver.find_element_by_id("user-name").send_keys(un)
@@ -100,6 +117,7 @@ class Execution(Chrome):
         obj.run()
         obj.login()
         obj.Run1()
+        obj.sidebar()
     elif (un == 'locked_out_user' and pw == 'secret_sauce'):
         obj = Chrome()
         obj.setUp()
